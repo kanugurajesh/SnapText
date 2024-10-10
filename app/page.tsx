329f8415164d95d3ba4741423d2c652370f180fb
kanugurajesh <kanugurajesh3@gmail.com>
2024-10-10 14:43:14 +0530

    Adding the page to the github

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,41 @@
+"use client";
+
 import Toggle from "@/components/toggle";
 import Link from "next/link";
 import Image from "next/image";
+import { useTheme } from "next-themes";
 
 export default function Home() {
+  const { theme, setTheme } = useTheme();
   return (
     <main className="p-4">
       <div className="float-right">
         <Toggle />
       </div>
-      <div className="mt-24 flex flex-col items-center gap-8 justify-center w-full">
+      <div className="mt-24 flex flex-col items-center gap-8 justify-center w-full max-w-2xl mx-auto">
         <Link
           href=""
-          className="text-sm border border-black rounded-full p-1 px-[20px] bg-white font-semibold gap-2 flex items-center justify-center dark:text-black"
+          className="text-sm border border-black bg-black text-white rounded-full p-1 px-[20px] dark:bg-white font-semibold gap-2 flex items-center justify-center dark:text-black"
         >
-          <span className="mb-[1px]">Watch Demo</span>
+          <span className="mb-[1px] leading-6">Watch Demo</span>
           <Image
-            src="./right-arrow.svg"
-            alt=""
-            width={15}
+            src={`${
+              theme == "dark" ? "./right-arrow-black" : "./right-arrow-white"
+            }.svg`}
+            alt="Play button"
+            width={14}
             height={10}
-            className="mt-[3px]"
+            className="mt-[2px]"
           />
         </Link>
-        <div className="flex flex-col text-3xl tracking-wide leading-tight text-center font-semibold max-w-64">
-          <span className="mx-auto inline-flex bg-gradient-to-r from-neutral-900 via-slate-500 to-neutral-500 bg-[200%_auto] bg-clip-text leading-tight text-transparent dark:from-neutral-100 dark:via-slate-400 dark:to-neutral-400">Download youtube transcript</span>
+        <div className="flex flex-col text-3xl tracking-wide leading-tight text-center font-semibold sm:text-6xl">
+          Download youtube transcript
         </div>
-        <p></p>
+        <p className="text-center text-lg leading-6 text-gray-600 dark:text-gray-200 font-medium">
+          Ready-to-use, simply paste the YouTube video link and easily download
+          the transcript and images. All functionalities crafted for seamless
+          integration.
+        </p>
         <div>
           <Link href=""></Link>
           <Link href=""></Link>
