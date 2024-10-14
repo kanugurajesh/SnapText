6d655fb6542db2e8b50ea6f05a0082d9ca3b264c
kanugurajesh <kanugurajesh3@gmail.com>
2024-10-14 11:40:55 +0530

    Adding the final project

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -137,17 +137,79 @@ const Home = () => {
               </div>
             </div>
             <div>
-              {showText && (
+              {showText ? (
                 <div className="overflow-y-scroll max-h-[70vh]">{textData}</div>
-              )}
-              {!showText && (
-                <pre
-                  className="overflow-y-scroll"
-                  style={{ maxHeight: "300px", maxWidth: "500px" }}
-                >
+              ) : (
+                <pre className="overflow-y-scroll max-h-[70vh] ">
                   {JSON.stringify(JSON.parse(transcript), null, 2)}
                 </pre>
               )}
+            </div>
+            <div className="flex gap-2 items-center justify-center">
+              <div
+                className={
+                  "p-2 rounded-md cursor-pointer transition-all ease-in-out duration-300 bg-black text-white hover:bg-gray-100 hover:text-black px-3"
+                }
+                onClick={() => {
+                  navigator.clipboard.writeText(
+                    showText ? textData : transcript
+                  );
+                  toast.success(
+                    `${showText ? "Text" : "Json"} Copied to clipboard`
+                  );
+                }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184"
+                  />
+                </svg>
+              </div>
+              <div
+                className={
+                  "p-2 rounded-md cursor-pointer transition-all ease-in-out duration-300 bg-black text-white hover:bg-gray-100 hover:text-black px-3"
+                }
+                onClick={() => {
+                  // write code to download the file text or json
+                  const element = document.createElement("a");
+                  const file = new Blob([showText ? textData : transcript], {
+                    type: showText ? "text/plain" : "application/json",
+                  });
+                  element.href = URL.createObjectURL(file);
+                  element.download = showText
+                    ? "transcript.txt"
+                    : "transcript.json";
+                  document.body.appendChild(element);
+                  element.click();
+                  toast.success(
+                    `${showText ? "Text" : "Json"} file downloaded`
+                  );
+                }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m9 13.5 3 3m0 0 3-3m-3 3v-6m1.06-4.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z"
+                  />
+                </svg>
+              </div>
             </div>
           </code>
         )}
