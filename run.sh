287b68c3c1042bdb0baa93c1286588e5821290f8
kanugurajesh <kanugurajesh3@gmail.com>
2024-10-14 17:35:10 +0530

    Adding the project to the github

--- run.sh
+++ run.sh
@@ -1,4 +1,15 @@
+#!/bin/bash
+
+# Run npm build
 npm run build
+if [ $? -ne 0 ]; then
+  echo "Build failed. Exiting..."
+  exit 1
+fi
+
+# Git commands
 git add .
-git commit -m "deploy"
+git commit -m "$1"  # Use $1 for the commit message passed from the command line
 git push -u origin main
+
+echo "All commands executed successfully."
