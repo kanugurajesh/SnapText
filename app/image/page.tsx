5d627216889203b07d2778a9245b5c1425f7b437
kanugurajesh <kanugurajesh3@gmail.com>
2024-10-14 16:56:49 +0530

    Removing the download feature

--- app/image/page.tsx
+++ app/image/page.tsx
@@ -3,8 +3,6 @@
 import { useState } from "react";
 import Image from "next/image";
 import toast, { Toaster } from "react-hot-toast";
-import Link from "next/link";
-import Loading from "@/components/loading";
 
 const Page = () => {
   const [src, setSrc] = useState<string>("");
@@ -120,26 +118,6 @@ const Page = () => {
               className="rounded-md cursor-pointer"
               onClick={() => setLImage(url)}
             />
-            <Link
-              href={url}
-              download
-              className="bg-white p-2 absolute bottom-0 right-0"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m9 13.5 3 3m0 0 3-3m-3 3v-6m1.06-4.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z"
-                />
-              </svg>
-            </Link>
           </div>
         ))}
       </div>
